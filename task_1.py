0455c955f406c14d87560f0f237983696cf242c2
stepanenkoVale4a <159696719+stepanenkoVale4a@users.noreply.github.com>
2024-03-17 17:20:11 +0200

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -4,17 +4,15 @@ file_src = Path("test.txt")
 def total_salary(file_src):
     try:
      with open(file_src, 'r') as file:
-     lines = file.readlines()
-    
-     total = 0 #Total sum
-     counter = 0
+    lines = file.readlines()
+total = 0 #Total sum
+counter = 0
 
-     for line in lines:
+for line in lines:
      _, amount = line.strip().split(',')
      total += int(amount)
      counter += 1
-
-     sr_zp = total / counter
-     result = (total, sr_zp)
+sr_zp = total / counter
+result = (total, sr_zp)
      return result
     except OSError as err:  print('Помилка доступу до файлу')
